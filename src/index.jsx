8c284c344940b6308b6e00d6dcd3afb27f406ad3
alexlbr <alex.lobera@gmail.com>
2017-06-08 13:50:00 +0100

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import withWidth, {
+  Width,
+  EXTRA_LARGE,
+  LARGE,
+  MEDIUM,
+  SMALL,
+  EXTRA_SMALL
+} from './components/Width'
+
+export { Width, EXTRA_LARGE, LARGE, MEDIUM, SMALL, EXTRA_SMALL }
+
+export default withWidth
